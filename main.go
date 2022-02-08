207315a8bdd8674bbcda9baaaad29c147695edce
thomascwei <thomas.c.wei@gmail.com>
2022-02-08 20:54:08 +0800

    .

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var IpHub map[string][]int64
+
+func webpage(w http.ResponseWriter, r *http.Request) {
+	// 取得IP
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	// 取得當下timestamp
+	now := time.Now().Unix()
+	// 判斷IP是否已被紀錄
+	if _, ok := IpHub[ip]; ok {
+		// 清除60秒前紀錄
+		for i, v := range IpHub[ip] {
+			if v >= now-60 {
+				IpHub[ip] = IpHub[ip][i:]
+				break
+			}
+		}
+		// 檢查當前長度, 超過顯示Error
+		if len(IpHub[ip]) >= 60 {
+			fmt.Fprintf(w, "Error")
+			return
+		} else { // 長度未超過, append
+			IpHub[ip] = append(IpHub[ip], now)
+			fmt.Fprintf(w, strconv.Itoa(len(IpHub[ip])))
+			return
+		}
+	// 不存在的IP新增
+	} else {
+		IpHub[ip] = []int64{now}
+		fmt.Fprintf(w, "1")
+		return
+	}
+
+}
+
+func main() {
+	IpHub = make(map[string][]int64)
+	http.HandleFunc("/", webpage)
+	http.ListenAndServe(":8888", nil)
+}
